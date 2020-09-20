314580c05f855506444365b7552e685f8fa88fbb
kwamkert <tomriddle477@gmail.com>
2020-09-20 21:20:08 +0000

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { AuthLayoutComponent, SuperAdminComponent } from './layouts';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', 
+    component: AuthLayoutComponent,
+    loadChildren: () => import('./modules/authentication/authentication.module')
+                       .then(m => m.AuthenticationModule)
+  },
+  
+  {
+    path:'admin', 
+    component: SuperAdminComponent,
+    loadChildren: () => import('./views/admin/admin.module')
+                       .then(m => m.AdminModule),
+    canActivate: [AuthGuard]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent, SidebarComponent, FooterComponent, AuthLayoutComponent, SuperAdminComponent, FleetManangerComponent, PageTitleComponent} from './layouts';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+//import { NgxUiLoaderModule } from 'ngx-ui-loader/lib/core/ngx-ui-loader.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+     SidebarComponent, 
+     FooterComponent, 
+     AuthLayoutComponent,
+      SuperAdminComponent, 
+      FleetManangerComponent,
+       PageTitleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    //NgxUiLoaderModule,
+      ToastrModule.forRoot({
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+  }), 
   ],
   providers: [],
   bootstrap: [AppComponent]
